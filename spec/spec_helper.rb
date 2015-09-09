ca190b69cdc8dbf6e8e4ddb971193ded2bddfb92
Sergey Prokopchuk <prokopchuksv21@gmail.com>
2015-09-09 21:31:50 +0300

    Initialize repository

---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+
+RSpec.configure do |config|
+   config.expect_with :rspec do |expectations|
+     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    Warden.test_reset!
+    DatabaseCleaner.clean
+  end
+end
+
+
